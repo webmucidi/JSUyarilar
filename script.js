e89475ad2d478dbb5c0e867ca3bdb218000b16ab
WebMucidi <59727557+webmucidi@users.noreply.github.com>
2022-04-25 11:29:54 +0000

    rastgele sözler

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let sozler=["Mutluluk evine giden yol, ızdırap bahçesinden geçer.","Emek olmadan, yemek olmaz.",""];
+let sozler=["Mutluluk evine giden yol, ızdırap bahçesinden geçer.","Emek olmadan, yemek olmaz.","El elden üstündür."];
+
+const buton=document.getElementById("btnDegistir");
+const sonuc=document.getElementById("soz");
+
+let sayi=Math.floor(Math.random() * 3);
+sonuc.innerHTML=sozler[sayi];
+
+buton.addEventListener("click",()=>{
+
+  sayi=Math.floor(Math.random() * 3);
+  sonuc.innerHTML=sozler[sayi];
+  
+})
