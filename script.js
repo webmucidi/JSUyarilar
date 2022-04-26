d0eb6e7870661ef5d9dcb8f466f90faa44de266a
WebMucidi <fatihcelik1907@gmail.com>
2022-04-26 14:49:53 +0300

    üst üste aynı söz geliyor

--- script.js
+++ script.js
@@ -2,13 +2,48 @@ let sozler=["Mutluluk evine giden yol, ızdırap bahçesinden geçer.","Emek olm
 
 const buton=document.getElementById("btnDegistir");
 const sonuc=document.getElementById("soz");
+const kapsayici=document.getElementById("kapsayici");
+const yazar=document.createElement("h5");
 
-let sayi=Math.floor(Math.random() * 3);
-sonuc.innerHTML=sozler[sayi];
+kapsayici.appendChild(yazar);
+
+const resim=document.createElement("img");
+document.getElementById("kapsayici").appendChild(resim);
+resim.setAttribute("src","images/anonim.jpg");
+
+
+sonuc.innerHTML=sozler[0];
+yazar.innerHTML="Anonim";
 
 buton.addEventListener("click",()=>{
-
-  sayi=Math.floor(Math.random() * 3);
-  sonuc.innerHTML=sozler[sayi];
   
+  let sayi=Math.floor(Math.random() * 3);
+  console.log(sayi);
+  sonuc.innerHTML=sozler[sayi];
+
+  let renk1=Math.round(Math.random()*255);
+  let renk2=Math.round(Math.random()*255);
+  let renk3=Math.round(Math.random()*255);
+
+  document.body.style.background="rgb("+renk1+","+renk2+","+renk3+")";
+
+  if(sayi==0){
+    yazar.innerHTML="Anonim";
+    resim.setAttribute("src","images/anonim.jpg");
+    kapsayici.style.background="white";
+   
+  }
+  else if(sayi==1){
+    yazar.innerHTML="Aşık Veysel";
+    resim.setAttribute("src","images/veysel.jpg");
+    kapsayici.style.background="rgb(252, 249, 251)";
+    
+  }
+  else if(sayi==2){
+    yazar.innerHTML="Yunus Emre";
+    kapsayici.setAttribute("src","images/yunus.jpg");
+    kapsayici.style.background="rgb(196, 174, 189)";
+    
+  }
 })
+
